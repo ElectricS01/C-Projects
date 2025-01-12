52fed9b45f10c1824dc77b0aedfee843b9c27fea
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2025-01-12 18:13:27 +1100

    Caesar Cipher lowercase

---
+++ caesar/main.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_number(const char *str) {
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (!isdigit(str[i]))
+      return 0;
+  }
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2 || !is_number(argv[1])) {
+    printf("Usage: ./caesar key\n");
+    return 1;
+  }
+
+  char text[64];
+  int success;
+  int total = 0;
+
+  printf("plaintext:  ");
+
+  success = scanf("%63[^\n]", text);
+
+  while (getchar() != '\n')
+    ;
+
+  int g = atoi(argv[1]) % 26;
+
+  for (int i = 0; text[i] != '\0'; i++) {
+    printf("%c", 'a' + ((text[i] - 'a' + g) % 26));
+  }
+
+  printf("\n");
+}
